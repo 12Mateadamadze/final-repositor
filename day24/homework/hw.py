a0bce7624b964a796ca1e230ecabccd3073ff53f
12Mateadamadze <159064219+12Mateadamadze@users.noreply.github.com>
2024-05-12 18:51:14 +0400

    mate commit

---
+++ day24/homework/hw.py
@@ -0,0 +1,16 @@
+#1
+# costumers = 10
+# while costumers > 0:
+#     print("ადგილი დაკავდა")
+#     costumers = costumers - 1
+
+
+
+
+
+
+
+#2
+goa = int(input("number: "))
+for i in range(goa):
+    print("hello world")
---
+++ day26/hoomework/hw.py
@@ -0,0 +1,7 @@
+password = "goa best"
+guess = input("enter password: ") 
+while guess != password:
+  guess = input("enter password:  ")
+print("password correct")
+
+
