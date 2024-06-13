35fd2812d5880916010777cbdaa9aeae938ae6b0
12Mateadamadze <159064219+12Mateadamadze@users.noreply.github.com>
2024-06-13 17:09:32 +0400

    commit

---
+++ day35/homework/hw.py
@@ -0,0 +1,8 @@
+my_cars = ["jigul","toyota_prius","hunday","wolsvagen","lamborgini","bmw","mersedes"]
+my_cars[0] = "buggati"
+my_cars[1] = "wrangler"
+my_cars[2] = "range rover"
+my_cars[3] = "audi"
+
+print(my_cars)
+print(my_cars[0] + " " + my_cars[1] + " " + my_cars[2] + " " + my_cars[3] + " " + my_cars[4] + " " + my_cars[5] + " " + my_cars[6])
